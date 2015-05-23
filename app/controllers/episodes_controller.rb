df744e4b7e84644a5855b70b150c20d54a663779
Hosnia Najem <hosnia.najem@hanseventures.com>
2015-05-23 22:56:32 +0200

    add genre and show details

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -7,13 +7,13 @@ class EpisodesController < ApplicationController
 
   def show
     @episode = OpenStruct.new Tmdb::Episode.detail(params[:tv_show_id],params[:season_number],params[:episode_number])
+    @genre = Tmdb::Genre.detail(params[:genre_id])
   end
 
 
   def load_episode
     @tv_show = OpenStruct.new Tmdb::TV.detail(params[:id])
     @tv_show.seasons = @tv_show.seasons.map { |season| OpenStruct.new season }
-
 
   end
 
