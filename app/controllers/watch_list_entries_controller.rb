335085961be2ef19dbde905f6ed2986ab8e4dd16
Anne Lamprecht <anne.lamprecht@hanseventures.com>
2015-05-23 20:14:16 +0200

    show watchlistentries only for current_user

--- app/controllers/watch_list_entries_controller.rb
+++ app/controllers/watch_list_entries_controller.rb
@@ -2,13 +2,13 @@ class WatchListEntriesController < ApplicationController
 
   def add
     WatchListEntry.where(watch_list_entry_params).first_or_create
-    redirect_to user_path(123)
+    redirect_to user_path(current_user)
   end
 
-private 
+private
 
   def watch_list_entry_params
-    params.permit(:imdb_identifier)
+    params.permit(:imdb_identifier, :user_id)
   end
 
 end
